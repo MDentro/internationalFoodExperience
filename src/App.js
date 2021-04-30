bae5bbf76c70156e1767c429841cf868ff9c004a
MDentro <michelle.dentro1@gmail.com>
2021-04-30 12:56:32 +0200

    add recipeDetailsPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import RecipeDetailsPage from "./pages/recipeDetailsPage/RecipeDetailsPage";
 
 function App() {
   return (
    <>
      <h1>International Food Experience</h1>
+      <RecipeDetailsPage/>
    </>
   );
 }
---
+++ src/components/helpers/createMeasuresArray.js
@@ -0,0 +1,87 @@
+function createMeasuresArray (meals) {
+    const measures = [];
+
+    if(meals.strMeasure1) {
+        measures.push(meals.strMeasure1)
+    }
+
+    if(meals.strMeasure2) {
+        measures.push(meals.strMeasure2)
+    }
+
+    if(meals.strMeasure3) {
+        measures.push(meals.strMeasure3)
+    }
+
+    if(meals.strMeasure4) {
+        measures.push(meals.strMeasure4)
+    }
+
+    if(meals.strMeasure5) {
+        measures.push(meals.strMeasure5)
+    }
+
+    if(meals.strMeasure6) {
+        measures.push(meals.strMeasure6)
+    }
+
+    if(meals.strMeasure7) {
+        measures.push(meals.strMeasure7)
+    }
+
+    if(meals.strMeasure8) {
+        measures.push(meals.strMeasure8)
+    }
+
+    if(meals.strMeasure9) {
+        measures.push(meals.strMeasure9)
+    }
+
+    if(meals.strMeasure10) {
+        measures.push(meals.strMeasure10)
+    }
+
+    if(meals.strMeasure11) {
+        measures.push(meals.strMeasure11)
+    }
+
+    if(meals.strMeasure12) {
+        measures.push(meals.strMeasure12)
+    }
+
+    if(meals.strMeasure13) {
+        measures.push(meals.strMeasure13)
+    }
+
+    if(meals.strMeasure14) {
+        measures.push(meals.strMeasure14)
+    }
+
+    if(meals.strMeasure15) {
+        measures.push(meals.strMeasure15)
+    }
+
+    if(meals.strMeasure16) {
+        measures.push(meals.strMeasure16)
+    }
+
+    if(meals.strMeasure17) {
+        measures.push(meals.strMeasure17)
+    }
+
+    if(meals.strMeasure18) {
+        measures.push(meals.strMeasure18)
+    }
+
+    if(meals.strMeasure19) {
+        measures.push(meals.strMeasure19)
+    }
+
+    if(meals.strMeasure20) {
+        measures.push(meals.strMeasure20)
+    }
+
+    return measures
+}
+
+export default createMeasuresArray;
