44a8108ce5e76b5742b44bedf343439718ea04b0
MDentro <michelle.dentro1@gmail.com>
2021-05-12 11:44:13 +0200

    regex added to SignUpPage

--- src/pages/signUpPage/SignUpPage.js
+++ src/pages/signUpPage/SignUpPage.js
@@ -18,34 +18,34 @@ function SignUpPage() {
                 <section>
                     <InputField
                         label="Username"
-                        fieldRef={register("username", {required: true})}
+                        fieldRef={register("username", {required: true, pattern: /[0-9a-zA-Z]{6,}/})}
                         name="username"
                         type="text"
                         id="username-sign-up-field"
                     />
-                    {errors.username && <p className="error">Username is required</p>}
+                    {errors.username && <p className="error">Username is required and should have a minimum of six characters with only letters and numbers.</p>}
                 </section>
 
                 <section>
                     <InputField
                         label={"Email"}
-                        fieldRef={register("email", {required: true})}
+                        fieldRef={register("email", {required: true, pattern: /\S+@\S+\.\S+/})}
                         name="email"
                         type="email"
                         id="email-sign-up-field"
                     />
-                    {errors.email && <p className="error">Email is required</p>}
+                    {errors.email && <p className="error">Email is required.</p>}
                 </section>
 
                 <section>
                     <InputField
                         label="Password"
-                        fieldRef={register("password", {required: true})}
+                        fieldRef={register("password", {required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/})}
                         name="password"
                         type="password"
                         id="password-sign-up-field"
                     />
-                    {errors.password && <p className="error">Password is required</p>}
+                    {errors.password && <p className="error">Password is required and should have a minimum of eight characters with at least one uppercase letter, one lowercase letter and one number.</p>}
                 </section>
 
                 <SubmitButton
