7b777da80cf6f18d09c1520ac8573d343d1f0f75
MDentro <michelle.dentro1@gmail.com>
2021-05-18 14:46:56 +0200

    styling RandomRecipePage and RecipePage. helping collors not yet removed

--- src/pages/randomRecipeSearchPage/RandomRecipeSearchPage.js
+++ src/pages/randomRecipeSearchPage/RandomRecipeSearchPage.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import RecipeDetails from "../../components/recipeDetails/RecipeDetails";
 import buildRecipeApiEndpoint from "../../helpers/buildRecipeApiEndpoint";
-import FunctionalButton from "../../components/buttons/functionalButton/FunctionalButton";
+import FunctionalButton from "../../components/buttons/functionalButton/FunctionalButton"
+import styles from "./RandomRecipeSearchPage.module.css"
 
 function RandomRecipeSearchPage() {
     const [idMeal, setIdMeal] = useState("");
@@ -15,7 +16,7 @@ function RandomRecipeSearchPage() {
             toggleErrorMessage(false);
             toggleLoading(true);
             try {
-                const { data : { meals }} = await axios.get(buildRecipeApiEndpoint("random", null, null, null));
+                const {data: {meals}} = await axios.get(buildRecipeApiEndpoint("random", null, null, null));
                 console.log("details random", meals[0]);
                 console.log("details random", meals[0].idMeal);
                 setIdMeal(meals[0].idMeal);
@@ -36,30 +37,35 @@ function RandomRecipeSearchPage() {
 
 
     return (
-        <>
-            <h1>Random Recipe</h1>
+        <div className={styles["random-page-container"]}>
+
+            <h2 className={styles["random-title-container"]}>Random Recipe</h2>
 
             {!errorMessage && !loading &&
             <>
-                <article>
-                    <p>Please try our random recipe for some inspiration. If you don't like the given recipe please
-                        press
-                        the button below and you'll get another one.</p>
-                </article>
+                <div className={styles["random-introduction-container"]}>
+                    <article>
+                        <p>Please try our random recipe for some inspiration. If you don't like the given recipe please
+                            press
+                            the button below and you'll get another one.</p>
+                    </article>
 
-                <FunctionalButton
-                    type="submit"
-                    handleClick={clickHandler}
-                >
-                    NEW RANDOM RECIPE
-                </FunctionalButton>
+                    <div className={styles["random-button"]}>
+                    <FunctionalButton
+                        type="submit"
+                        handleClick={clickHandler}
+                    >
+                        NEW RANDOM RECIPE
+                    </FunctionalButton>
+                    </div>
+                </div>
 
-                <RecipeDetails idMeal={idMeal} />
+                <RecipeDetails idMeal={idMeal}/>
             </>
-              }
+            }
             {errorMessage && <span>Something went wrong with fetching the data, please try again later.</span>}
             {loading && <span>Loading...</span>}
-        </>
+        </div>
     );
 }
 
--- src/pages/recipeDetailsPage/RecipeDetailsPage.js
+++ src/pages/recipeDetailsPage/RecipeDetailsPage.js
@@ -1,16 +1,17 @@
 import React from "react";
 import RecipeDetails from "../../components/recipeDetails/RecipeDetails";
 import { useParams } from "react-router-dom";
+import styles from "./RecipeDetailsPage.module.css"
 
 
 function RecipeDetailsPage() {
     const params = useParams();
 
     return (
-     <>
+     <div className={styles["recipe-page-container"]}>
         <RecipeDetails idMeal={params.idMeal} />
          {console.log("is dit undefined", params)}
-     </>
+     </div>
     );
 }
 
